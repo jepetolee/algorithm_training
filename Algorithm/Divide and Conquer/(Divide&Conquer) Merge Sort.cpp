ffa406d453eeee253eab7b4f41d08d0734a0b19a
jepetolee <55366212+jepetolee@users.noreply.github.com>
2021-06-29 17:49:49 +0900

    fixed ascii

--- Algorithm/Divide and Conquer/(Divide&Conquer) Merge Sort.cpp
+++ Algorithm/Divide and Conquer/(Divide&Conquer) Merge Sort.cpp
@@ -6,15 +6,17 @@
 using namespace std;
 
 int sorted[99999];
-void merge(int arr[],int sorted[], int start, int median, int end) {
-	int i = start,k = start;
+void merge(int arr[], int sorted[], int start, int median, int end)
+{
+	int i = start, k = start;
 	int j = median + 1;
-	while (i<=median&& j<= end) {
-		
-		if (arr[i] <= arr[j]) 
+	while (i <= median && j <= end)
+	{
+
+		if (arr[i] <= arr[j])
 			sorted[k++] = arr[i++];
-			
-		else 
+
+		else
 			sorted[k++] = arr[j++];
 	}
 	int tmp = i > median ? j : i;
@@ -26,29 +28,32 @@ void merge(int arr[],int sorted[], int start, int median, int end) {
 		arr[i] = sorted[i];
 }
 
-void merge_sort(int arr[], int start, int end) {
-	
-	if (start < end) {
+void merge_sort(int arr[], int start, int end)
+{
+
+	if (start < end)
+	{
 		int median = (start + end) / 2;
 		merge_sort(arr, start, median);
-		merge_sort(arr, median+1, end);
-		merge(arr,sorted, start, median, end);
+		merge_sort(arr, median + 1, end);
+		merge(arr, sorted, start, median, end);
 	}
 }
 
-int main() {
+int main()
+{
 	int length;
-	cout << "배열의 길이 입력:";
+	cout << "length of array:";
 	cin >> length;
 	int *arr = new int[length];
-	
-	for (int i = 0; i < length; i++)
-		cin >> arr[i];
-	
-	merge_sort(arr, 0, length-1);
 
 	for (int i = 0; i < length; i++)
-	cout << arr[i]<< " ";
+		cin >> arr[i];
+
+	merge_sort(arr, 0, length - 1);
+
+	for (int i = 0; i < length; i++)
+		cout << arr[i] << " ";
 
 	delete[] arr;
 }
--- Algorithm/Divide and Conquer/(Divide&Conquer) Quick Sort.cpp
+++ Algorithm/Divide and Conquer/(Divide&Conquer) Quick Sort.cpp
@@ -5,26 +5,30 @@
 
 using namespace std;
 
-void quick_sort(int arr[], int start, int end) {
-	if (start > end) return ;
+void quick_sort(int arr[], int start, int end)
+{
+	if (start > end)
+		return;
 	int pivot = start;
 	int i = pivot + 1;
 	int j = end;
-	
+
 	int temp;
-	while (i <= j) {
-		
+	while (i <= j)
+	{
 
 		while (i <= end && arr[i] <= arr[pivot])
 			i++;
 		while (j > start && arr[j] >= arr[pivot])
 			j--;
-		if (i > j) {
+		if (i > j)
+		{
 			temp = arr[j];
 			arr[j] = arr[pivot];
 			arr[pivot] = temp;
 		}
-		else {
+		else
+		{
 			temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
@@ -32,22 +36,23 @@ void quick_sort(int arr[], int start, int end) {
 	}
 
 	quick_sort(arr, start, j - 1);
-	quick_sort(arr, j+1, end);
+	quick_sort(arr, j + 1, end);
 }
 
-int main() {
+int main()
+{
 	int length;
-	cout << "배열의 길이 입력:";
+	cout << "length of array:";
 	cin >> length;
 	int *arr = new int[length];
-	
-	for (int i = 0; i < length; i++)
-		cin >> arr[i];
-	
-	quick_sort(arr, 0, length-1);
 
 	for (int i = 0; i < length; i++)
-	cout << arr[i]<< " ";
+		cin >> arr[i];
+
+	quick_sort(arr, 0, length - 1);
+
+	for (int i = 0; i < length; i++)
+		cout << arr[i] << " ";
 
 	delete[] arr;
 }
